cfaf306bd67edb90f6c0224a2baea56662ec60fa
jamshid98 <jamshid98991213@gmail.com>
2025-09-21 19:29:52 +0900

    feat: develop memberschema model

---
+++ src/libs/enums/member.enum.ts
@@ -0,0 +1,13 @@
+
+
+export enum MemberType {
+    USER = "USER",       // oddiy xaridor
+    SELLER = "SELLER",   // sotuvchi / brend
+    ADMIN = "ADMIN"      // admin
+}
+
+export enum MemberStatus {
+    ACTIVE = "ACTIVE",
+    BLOCK = "BLOCK",
+    DELETE = "DELETE",
+}
