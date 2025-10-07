bd1c21828866c5063a648c45a47e03ec508cdaea
jamshid98 <jamshid98991213@gmail.com>
2025-10-08 04:01:29 +0900

    feat: develop frontend Users Page (ejs)

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,30 +1,54 @@
-const { response } = require("express");
+console.log("Users frontend JS loaded");
 
-console.log("Users frontend javascript file");
+$(function() {
+  const $tableBody = $("#user-table-body");
+  const $rows = $tableBody.find("tr");
 
-
-$(function () {
-    $(".member-status").on("change", function (e) {
-        const id = e.target.id,
-         memberStatus = $ (`#${id}.member-status`).val();
-      
-
-        // TODO Axios updateChosenUser
-        axios.post("/admin/user/edit", {
-            _id: id,
-            memberStatus: memberStatus,
-        }).then((response) => {
-            console.log("response:", response);
-            const result = response.data;
-           
-
-            if (result.data) {
-                $(".member-status").blur();
-            } else alert("User update failed!");
-        })
-        .catch((err) => {
-            console.log(err);
-            alert("User update failed");
-        });
+  // Search
+  function filterUsers() {
+    const query = $("#search-input").val().toLowerCase();
+    $rows.each(function() {
+      const name = $(this).find(".user-name").text().toLowerCase();
+      $(this).toggle(name.includes(query));
     });
+  }
+
+  $("#search-btn").on("click", filterUsers);
+  $("#search-input").on("keyup", filterUsers);
+  $("#reset-btn").on("click", function() {
+    $("#search-input").val("");
+    $rows.show();
+  });
+
+  // Status update (React-style)
+  $(".member-status").on("change", function() {
+    const $select = $(this);
+    const id = $select.data("id");
+    const newStatus = $select.val();
+
+    axios.post("/admin/user/edit", { _id: id, memberStatus: newStatus })
+      .then(res => {
+        // backenddan success/ok qaytsa ham alert bermaymiz
+        if(res.data.error) {
+          alert("Error updating user!"); // faqat xato bo‘lsa
+          $select.val($select.data("prev")); // oldingi statusga qaytaradi
+        } else {
+          $select.data("prev", newStatus); // yangi statusni saqlaydi
+          console.log(`User ${id} status updated to ${newStatus}`);
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        $select.val($select.data("prev"));
+        alert("Error updating user!");
+      });
+  });
+
+  // Har selectga oldingi statusni saqlab qo‘yish
+  $(".member-status").each(function() {
+    $(this).data("prev", $(this).val());
+  });
 });
+
+
+
