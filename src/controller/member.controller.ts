2442ea4fc899de29dd75259968c9d92cb1056da8
jamshid98 <jamshid98991213@gmail.com>
2025-10-19 13:01:45 +0900

    feat: develop token creation via jwt

--- src/controller/member.controller.ts
+++ src/controller/member.controller.ts
@@ -3,18 +3,21 @@ import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { MemberInput,  Member, LoginInput } from "../libs/types/member";
 import Errors from "../libs/types/Errors";
+import AuthService from "../models/Auth.service";
 
 
 
 const memberService = new MemberService();
-
+const authService = new AuthService;
 const memberController: T = {};
 
 memberController.signup = async (req: Request, res: Response) => {
     try {
         console.log('signup');
         const input: MemberInput = req.body,
-            result: Member = await memberService.signup(input);
+            result: Member = await memberService.signup(input),
+                token = await authService.createToken(result);
+                 console.log("token=>", token)
 
         res.json({ member: result });
     } catch (err) {
@@ -28,8 +31,11 @@ memberController.login = async (req: Request, res: Response) => {
     try {
         console.log("login");
         const input: LoginInput = req.body,
-            result = await memberService.login(input);
-           
+            result = await memberService.login(input),
+            token = await authService.createToken(result);
+          console.log("token=>", token)
+        //TUDO: TOKENS Authentification
+        
         res.json({ member: result });
     } catch (err) {
         console.log("Error, login:", err);
