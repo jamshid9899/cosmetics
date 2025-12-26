c9ee1ad06d1f277f959e80ac901bab8833bc1009
jamshid98 <jamshid98991213@gmail.com>
2025-12-27 07:30:11 +0900

    feat:deploy project via shell

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #PRODUCTION
-# git reset --hard
-# git checkout master
-# git pull origin master
+ git reset --hard
+ git checkout master
+ git pull origin master
 
 npm i
 npm run build
